b2819bfca9e40120956460e789f24af6652ba186
sanyuktadahiwal <sanyuktadahiwal7@gmail.com>
2026-03-18 14:10:07 +0530

    Program to find the average mark

---
+++ Average.java
@@ -0,0 +1,9 @@
+public class Average {
+	public static void main(String[] args) {
+		int maths = 94;
+		int physics = 95;
+		int chemistry = 96;
+		int average = (maths+chemistry+physics)/3;
+		System.out.println("Sam’s average mark in PCM is " + average);
+	}
+}
